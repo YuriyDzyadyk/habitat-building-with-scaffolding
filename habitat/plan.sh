5b7b4e61a1a9053523d982b5a796bd4278aafde2
YuriyDzyadyk <yuraa_@ukr.net>
2022-09-22 14:27:35 +0300

    added imagemagick dependency with JPEG support

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,4 +2,4 @@ pkg_origin=yd_initials_tryhabitat
 pkg_name=meme-machine
 pkg_version="0.1.0"
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_deps=( core/imagemagick )
+pkg_deps=( yd_initials_tryhabitat/imagemagick )
